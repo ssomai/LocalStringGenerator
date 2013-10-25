21adffb1a7e58f8f73846bc5f9c2b73c1bf46cfc
ssomai <ssomai@gmail.com>
2013-10-25 16:51:14 +0900

    tetette
    eeeee

--- LocalStringGenerator_TestAndroid/gen/com/jnm/github/localstring/testandroid/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.jnm.github.localstring.testandroid;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
